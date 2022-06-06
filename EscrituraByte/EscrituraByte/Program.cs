20c01998365b9308df7394cf1ca36226a1628cc7
cristianHernandezVITT <105743030+cristianHernandezVITT@users.noreply.github.com>
2022-06-06 11:10:10 -0700

    Add files via upload

---
+++ EscrituraByte/EscrituraByte/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace EscrituraByte
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream fs = null;
+            byte[] buffer = new byte[81];
+            int nbytes = 0,car;
+            try
+            {
+                fs = new FileStream("Text.txt", FileMode.Create, FileAccess.Write);
+                Console.WriteLine("Escriba el texto que desea almacenar en el archivo: ");
+                while ((car = Console.Read()) != '\n' && (nbytes < buffer.Length))
+                {
+                    buffer[nbytes] = (byte)car;
+                    nbytes++;
+                }
+                fs.Write(buffer, 0, nbytes);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (fs != null) fs.Close();
+            }
+        }
+    }
+}
